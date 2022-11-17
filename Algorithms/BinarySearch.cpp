ce8d01d5488be1a15de4d0205252f3605177ffc2
Mher21 <mher.araqelyan.ec@gmail.com>
2022-11-17 21:10:22 +0400

    Binary Search updated

--- Algorithms/BinarySearch.cpp
+++ Algorithms/BinarySearch.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 void print(int x[]){
-   int n=sizeof(x)/sizeof(x[0]);
+    int n=sizeof(x)/sizeof(x[0]);
     for(int i=0;i<n;i++){
         cout<<"x["<<i<<"]="<<x[i]<<endl;
         }
@@ -15,26 +15,30 @@ void print(int x[]){
         }
         
     int  bin_srch(int x[],int low,int high, int value){
-        int mid=(low+high)/2;
-        if(x[mid]==value){
+        if(low<=high && value>=low && value<=high){
+            int mid=(low+high)/2;
+            if(x[mid]==value){
             return mid;
+            }
+            if(x[mid]>value){
+              return bin_srch(x,low,mid-1,value);
+            }
+            if(x[mid]<value){
+              return bin_srch(x,mid+1,high,value);
+            }
         }
-        if(x[mid]>value){
-            bin_srch(x,low,mid-1,value);
-        }
-        else{
-            bin_srch(x,mid+1,high,value);
-        }
+        return -1;
     }
         int main(){
-            int n;
+            int fn,n;
             cout<<"Enter Number of array elements: ";
             cin>>n;
             int x[n];
             input(x,n);
-            cout<<bin_srch(x,0,n-1,45)<<endl;
+            cout<<"Enter element for search: ";cin>>fn;
+            cout<<"Index of element is: "<<bin_srch(x,0,n-1,fn)<<endl;
+            return 0;
             //print(x);
-
 
         }
     
