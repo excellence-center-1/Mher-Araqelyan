7da9bf9ce54277e681708555064d9d83d8f3791f
Mher Araqelyan <excellencecenter001@gmail.com>
2023-04-14 13:50:06 +0400

    homework

--- Database/14-04-2023/Procedure.sql
+++ Database/14-04-2023/Procedure.sql
@@ -26,3 +26,25 @@ begin
 
 end; $$;
 
+/*Exercise 2:
+Create a procedure that accepts a product ID and returns the name of the product and the total revenue generated from that product.*/
+
+create or replace procedure get_info2(prod_id SMALLINT)
+language plpgsql
+as $$
+DECLARE
+    pname varchar(25);
+    squantity smallint;
+    pprice smallint;
+    total_amount smallint;
+begin
+        SELECT INTO pname name FROM product WHERE id=prod_id;
+        RAISE NOTICE 'Product Name: %', pname;
+
+        select into squantity sum(quantity) from "order" where  product_id=prod_id ;
+        select into pprice price from product where id=prod_id;
+        select into total_amount pprice*squantity;
+        RAISE NOTICE 'Total Amount Spent of this product: %', total_amount;
+
+end; $$;
+
