91a89ac21a20b8ff6bac43596a14d9cd75f8a9ea
Mher Araqelyan <excellencecenter001@gmail.com>
2023-05-20 18:41:25 +0400

    update homework

--- Web/20-05-2023/js_fetch.js
+++ Web/20-05-2023/js_fetch.js
@@ -1,3 +1,10 @@
+/* Create a function called fetchData that simulates fetching data from an API. The function should take a URL as a parameter and return a promise. The promise should resolve with the data fetched from the API if the request is successful, and reject with an error message if the request fails.
+Next, demonstrate the usage of the fetchData function by calling it with a sample URL using async/await syntax. Handle any errors that may occur during the asynchronous operation
+-------------------------------------
+Example usage
+fetchDataAsync('https://api.example.com/data'); // Data: { id: 1, name: 'John Doe', email: 'johndoe@example.com' }
+fetchDataAsync('https://api.example.com/dataaaa'); // Error: Error: Failed to fetch data 
+*/
 function fetchData(url) {
     return new Promise((resolve, reject) => {
         let byindex = false;
