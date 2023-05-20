eb068fd01157f0e4db411d5b26a5f6bdc95a341e
Mher Araqelyan <excellencecenter001@gmail.com>
2023-05-20 18:15:34 +0400

    homework

--- Web/20-05-2023/js_fetch.js
+++ Web/20-05-2023/js_fetch.js
@@ -1,24 +1,39 @@
 function fetchData(url) {
-  return new Promise((resolve, reject) => {
-    fetch(url)
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error('Failed to fetch data');
+
+    return new Promise((resolve, reject) => {
+        let byindex = false;
+        let index;
+        const lastChar = url[url.length - 1];
+        if (!isNaN(lastChar)) {
+            index = Number(lastChar);
+            url = url.slice(0, url.length - 2);
+            byindex = true;
         }
-      })
-      .then(data => resolve(data))
-      .catch(error => reject(error.message));
-  });
+        fetch(url)
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('Failed to fetch data');
+                }
+            })
+            .then(data => {
+                if (byindex) {
+                    const foundObject = data.find(item => item.id == index);
+                    resolve(foundObject);
+                } else {
+                    resolve(data);
+                }
+            })
+            .catch(error => reject(error.message));
+    });
 }
+
 async function fetchDataAsync(url) {
-  try {
-    const data = await fetchData(url);
-    console.log('Data:', data);
-  } catch (error) {
-    console.error('Error:', error);
-  }
+    try {
+        const data = await fetchData(url);
+        console.log('Data:', data);
+    } catch (error) {
+        console.error('Error:', error);
+    }
 }
-fetchDataAsync('https://api.example.com/data'); // Data: { id: 1, name: 'John Doe', email: 'johndoe@example.com' }
-fetchDataAsync('https://api.example.com/dataaaa'); // Error: Error: Failed to fetch data
